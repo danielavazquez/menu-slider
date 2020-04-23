8bc76c8558f3dbb0ccc523fbece20c280b1fdd7a
Daniela Vazquez <daniela_056@hotmail.com>
2020-04-22 21:42:12 -0600

    Add JavaScript and key frame styling for modal fade in fade out animation

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,18 @@
-document.addEventListener("DOMContentLoaded", () => {
-  //On loaded
-});
+const toggle = document.getElementById('toggle');
+const close = document.getElementById('close');
+const open = document.getElementById('open');
+const modal = document.getElementById('modal');
+
+//Toggle nav
+toggle.addEventListener('click', () => document.body.classList.toggle('show-nav'));
+
+//Show modal
+open.addEventListener('click', () => modal.classList.add('show-modal'));
+
+//Close button hide modal
+close.addEventListener('click', () => modal.classList.remove('show-modal'));
+
+//Hide modal on outside click
+//window object is the topmost object in browser, add eListener, take e the event parameter and 
+//used ternary operator
+window.addEventListener('click', e => e.target == modal ? modal.classList.remove('show-modal') : false);
